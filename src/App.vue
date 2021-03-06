88b21a4343b6b22a2a90c6a8b0a0acbfdc1dfeb9
lfchiu <c2h3i4u5@gmail.com>
2021-03-06 23:23:02 +0800

    A frond-end project (instagram)

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <the-header></the-header>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import theHeader from "./components/header.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { theHeader },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  font-style: none;
 }
 </style>
---
+++ src/components/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <div class="title">
+      <img
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+      />
+    </div>
+    <div class="search"></div>
+    <div class="home"></div>
+    <div class="message"></div>
+    <div class="explore"></div>
+    <div class="information"></div>
+    <div class="aboutMe"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.header {
+  width: 100%;
+  height: 200px;
+  background-color: aquamarine;
+  display: flex;
+  align-items: center;
+}
+.title {
+  width: 200px;
+  background-color: brown;
+}
+</style>
